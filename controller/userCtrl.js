e2360b9c4a4c4f0cb53305683e5c8c5df4caac11
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-03-30 01:09:56 +0600

    working on user login api

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel")
 const asyncHandler = require("express-async-handler")
+const { generateToken } = require("../config/jwtToken")
 
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email
@@ -14,4 +15,22 @@ const createUser = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { createUser }
+const loginUserCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    // checking if the user already exists
+    const findUser = await User.findOne({ email })
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+        res.json({
+            _id: findUser?._id,
+            firstName: findUser?.firstName,
+            lastName: findUser?.lastName,
+            email: findUser?.email,
+            token: generateToken(findUser?._id)
+        })
+    } else {
+        throw new Error("Invalid username or password")
+    }
+
+})
+
+module.exports = { createUser, loginUserCtrl }
