df7e66710499a8e1c8c308be3e797a66b05b040a
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-03-30 21:04:41 +0600

    working on user block unblock

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -36,10 +36,10 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
 
 // update a user
 const updateAnUser = asyncHandler(async (req, res) => {
-    const { id } = req.params
+    const { _id } = req.user
     try {
         const updatedUser = await User.findByIdAndUpdate(
-            id, {
+            _id, {
             firstName: req?.body?.firstName,
             lastName: req?.body?.lastName,
             email: req?.body?.email,
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { createUser, loginUserCtrl, getallUser, getaSingleUser, deleteAnSingleUser, updateAnUser } = require("../controller/userCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router()
 
 router.post("/register", createUser)
 router.post("/login", loginUserCtrl)
-router.put("/:id", updateAnUser)
 router.get("/all-users", getallUser)
-router.get("/:id", getaSingleUser)
+router.get("/:id", authMiddleware, isAdmin, getaSingleUser)
+router.put("/edit-user", authMiddleware, updateAnUser)
 router.delete("/:id", deleteAnSingleUser)
 
 module.exports = router;
