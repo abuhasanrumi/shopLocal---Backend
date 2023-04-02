5c2516b900023fdf9db01a430f38b2e921c053aa
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-03 01:57:01 +0600

    working on product section

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 const dotenv = require('dotenv').config()
 const dbConnect = require("./config/dbConnect");
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
-const authRouter = require("./routes/authRoute")
+const authRouter = require("./routes/authRoute");
+const productRouter = require("./routes/productRoute");
 
 const PORT = process.env.PORT || 4000;
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use("/api/user", authRouter)
+app.use("/api/product", productRouter)
 
 app.use(notFound)
 app.use(errorHandler)
