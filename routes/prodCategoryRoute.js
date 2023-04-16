9560781844d70f3669da61515396cf48bd62d7ee
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-17 01:23:54 +0600

    brand lsit api integration done

--- routes/prodCategoryRoute.js
+++ routes/prodCategoryRoute.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.post("/", authMiddleware, isAdmin, createProdCategory)
 router.put("/:id", authMiddleware, isAdmin, updateProdCategory)
 router.delete("/:id", authMiddleware, isAdmin, deleteProdCategory)
-router.get("/:id", authMiddleware, isAdmin, getProdCategory)
-router.get("/", authMiddleware, isAdmin, getAllProdCategory)
+router.get("/:id", getProdCategory)
+router.get("/", getAllProdCategory)
 
 module.exports = router
