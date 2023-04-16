dc8b47df68d29c7a4a4b7c9447b75a70ff282c16
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-04-17 00:59:15 +0600

    brand lsit api integration done

--- routes/brandRoute.js
+++ routes/brandRoute.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.post("/", authMiddleware, isAdmin, createBrand)
 router.put("/:id", authMiddleware, isAdmin, updateBrand)
 router.delete("/:id", authMiddleware, isAdmin, deleteBrand)
-router.get("/:id", authMiddleware, getBrand)
-router.get("/", authMiddleware, getAllBrand)
+router.get("/:id", getBrand)
+router.get("/", getAllBrand)
 
 module.exports = router
